3661da379691d9622cfeb823409d322ab1137245
zhangwei <pknfe@outlook.com>
2019-03-02 11:48:28 +0800

    1. 通过 SpringPipelineFactory 获取 Pipline 处理器时判断 bean 是否存在于 BeanFactory 中

--- gecco-spring-boot-autoconfigure/src/main/java/com/geccocrawler/boot/gecco/spider/SpringPipelineFactory.java
+++ gecco-spring-boot-autoconfigure/src/main/java/com/geccocrawler/boot/gecco/spider/SpringPipelineFactory.java
@@ -16,10 +16,18 @@ public class SpringPipelineFactory implements PipelineFactory, ApplicationContex
 
     private ApplicationContext context;
 
+    private final Class<Pipeline> pip = Pipeline.class;
+
     @Override
     public Pipeline<? extends SpiderBean> getPipeline(String s) {
         ObjectUtil.checkNotNull(s, "pipelineName");
-        return context.getBean(s, Pipeline.class);
+        String [] beanNames = context.getBeanNamesForType(pip);
+        for(String name : beanNames){
+            if(name.equals(s)){
+                return context.getBean(s, pip);
+            }
+        }
+        return null;
     }
 
     @Override
